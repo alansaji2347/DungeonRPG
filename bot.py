0e0381ea5b37c956ae15c1a7a5e60f8d2406dd08
Alan <tuj93905@temple.edu>
2023-02-16 17:30:49 -0500

    Updated the files for the bot to do basic responses

---
+++ bot.py
@@ -0,0 +1,44 @@
+import random
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:  # Try to send a message
+        response = responses.get_response(user_message)  # Get the response from the user's message
+
+        # Send the response to the user
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTA3NTg0NjQ5MTE4MjIyMzQ5MA.GbLHP0.NWt0FZ6MxyoI_QivtH00p068a9Q6T1OsOgzjTc'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} sent a message in {channel}: {user_message}')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # "?Help" -> "Help", i.e., ignores the question mark
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
